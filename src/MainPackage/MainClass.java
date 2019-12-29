1416de8d07c5530bcab576dabd227e735c1d0c6c
Neil Taiwan <neil.taiwan1999@gmail.com>
2019-12-30 00:17:08 +0800

    測試第二次上傳

--- src/MainPackage/MainClass.java
+++ src/MainPackage/MainClass.java
@@ -5,15 +5,16 @@ import javax.swing.*;
 public class MainClass {
 
     public MainClass() {
-    	System.out.println();
         JFrame frame = new JFrame();
         GamePanel gamePanel = new GamePanel();
 
+    	System.out.println("test2");
         frame.add(gamePanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Snacke Game");
         frame.setLocationRelativeTo(null);
 
+    	System.out.println("test2");
         frame.pack();
         frame.setVisible(true);
     }
